de0bd1a129167c55dfc3b1398c0f5272e70adc11
ethanlee <lee3159@purdue.edu>
2022-01-20 22:50:06 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Ethan Lee on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
